8b1f4e990d570d4a7b8c94bbefa0934230214a09
Radomir Epur <radomir9720@gmail.com>
2024-12-13 20:26:52 +0200

    chore(GeneralDataCubit): implement merge logic for speed and gear

--- lib/domain/data_source/blocs/general_data_cubit.dart
+++ lib/domain/data_source/blocs/general_data_cubit.dart
@@ -30,11 +30,53 @@ final class IntWithStatus {
   final int value;
   final PeriodicValueStatus status;
 
+  IntWithStatus copyWith({
+    int? value,
+    PeriodicValueStatus? status,
+  }) {
+    return IntWithStatus(
+      value: value ?? this.value,
+      status: status ?? this.status,
+    );
+  }
+
   Map<String, dynamic> toMap() {
     return {
       'value': value,
       'status': status.id,
     };
+  }
+}
+
+typedef _IntValueModifier = (
+  int Function(int a, int b),
+  int Function(int foldValue, int length),
+);
+
+extension _SequenceExt on Sequence<IntWithStatus> {
+  static int avgFold(int a, int b) => a + b;
+  static int maxFold(int a, int b) => a > b ? a : b;
+
+  static _IntValueModifier avg =
+      (avgFold, (foldValue, length) => foldValue ~/ length);
+
+  IntWithStatus merged([_IntValueModifier? valueModifier]) {
+    final modifier = valueModifier ?? avg;
+    if (length == 1) {
+      return first.copyWith(value: modifier.$2(first.value, 1));
+    }
+
+    //
+    final (foldValue, status) = fold(
+      (0, PeriodicValueStatus.normal),
+      (a, b) =>
+          (modifier.$1(a.$1, b.value), a.$2.id > b.status.id ? a.$2 : b.status),
+    );
+    //
+    return IntWithStatus(
+      value: modifier.$2(foldValue, length),
+      status: status,
+    );
   }
 }
 
@@ -66,8 +108,14 @@ final class GeneralDataState with EquatableMixin {
           const IntWithStatus.initial(),
         ),
         odometer = const IntWithStatus.initial(),
-        speed = const IntWithStatus.initial(),
-        gear = MotorGear.unknown;
+        speed = Sequence.fill(
+          hardwareCount.motors,
+          const IntWithStatus.initial(),
+        ),
+        gear = Sequence.fill(
+          hardwareCount.motors,
+          MotorGear.unknown,
+        );
 
   factory GeneralDataState.fromMap(Map<String, dynamic> map) {
     return GeneralDataState(
@@ -79,44 +127,37 @@ final class GeneralDataState with EquatableMixin {
         map.tryParseAndMapList('batteryPercent', IntWithStatus.fromMap),
       ),
       odometer: map.parseAndMap('odometer', IntWithStatus.fromMap),
-      speed: map.parseAndMap('speed', IntWithStatus.fromMap),
-      gear: map.parseAndMap('gear', MotorGear.fromId),
+      speed: Sequence.fromIterable(
+        map.tryParseAndMapList('speed', IntWithStatus.fromMap),
+      ),
+      gear: Sequence.fromIterable(
+        map.tryParseAndMapList('gear', MotorGear.fromId),
+      ),
     );
   }
 
   final HardwareCount hardwareCount;
   final Sequence<IntWithStatus> power;
   final IntWithStatus odometer;
-  final IntWithStatus speed;
-  final MotorGear gear;
+  final Sequence<IntWithStatus> speed;
+  final Sequence<MotorGear> gear;
   final Sequence<IntWithStatus> batteryPercent;
 
-  IntWithStatus get mergedBatteryPercent {
-    if (batteryPercent.length == 1) return batteryPercent.first;
-    //
-    final (levelSum, status) =
-        batteryPercent.fold((0, PeriodicValueStatus.normal), (a, b) {
-      return (a.$1 + b.value, a.$2.id > b.status.id ? a.$2 : b.status);
-    });
-    //
-    return IntWithStatus(
-      value: levelSum ~/ batteryPercent.length,
-      status: status,
-    );
-  }
+  IntWithStatus get mergedBatteryPercent => batteryPercent.merged();
 
-  IntWithStatus get mergedPower {
-    if (power.length == 1) return power.first;
-    //
-    final (powerSum, status) = power.fold(
-      (0, PeriodicValueStatus.normal),
-      (a, b) => (a.$1 + b.value, a.$2.id > b.status.id ? a.$2 : b.status),
-    );
-    //
-    return IntWithStatus(
-      value: powerSum ~/ power.length,
-      status: status,
-    );
+  IntWithStatus get mergedPower => power.merged();
+
+  IntWithStatus get mergedSpeed => speed.merged(
+        (
+          _SequenceExt.maxFold,
+          (foldValue, length) => foldValue ~/ 10,
+        ),
+      );
+
+  MotorGear get mergedGear {
+    if (gear.length == 1) return gear.first;
+    if (gear.toSet().length == 1) return gear.first;
+    return MotorGear.unknown;
   }
 
   @override
@@ -132,8 +173,8 @@ final class GeneralDataState with EquatableMixin {
     Sequence<IntWithStatus>? power,
     Sequence<IntWithStatus>? batteryPercent,
     IntWithStatus? odometer,
-    IntWithStatus? speed,
-    MotorGear? gear,
+    Sequence<IntWithStatus>? speed,
+    Sequence<MotorGear>? gear,
   }) {
     return GeneralDataState(
       hardwareCount: hardwareCount,
@@ -148,11 +189,11 @@ final class GeneralDataState with EquatableMixin {
   Map<String, dynamic> toMap() {
     return {
       'hardwareCount': hardwareCount.toMap(),
-      'power': power.map((e) => e.toMap()).toList(),
-      'batteryPercent': batteryPercent.map((e) => e.toMap()).toList(),
+      'power': [for (final e in power) e.toMap()],
+      'batteryPercent': [for (final e in batteryPercent) e.toMap()],
       'odometer': odometer.toMap(),
-      'speed': speed.toMap(),
-      'gear': gear.id,
+      'speed': [for (final e in speed) e.toMap()],
+      'gear': [for (final e in gear) e.id],
     };
   }
 }
@@ -186,23 +227,27 @@ class GeneralDataCubit extends Cubit<GeneralDataState> with ConsumerBlocMixin {
             ),
           );
         })
-        ..voidOnModel<MotorGearAndRoll,
-            MotorGearAndRollIncomingDataSourcePackage>((model) {
+        ..voidOnPackage<MotorGearAndRoll,
+            MotorGearAndRollIncomingDataSourcePackage>((package) {
           emit(
             state.copyWith(
-              gear: MotorGear.unknown,
+              gear: state.gear.updateAt(
+                package.motorIndex,
+                package.dataModel.gear,
+              ),
             ),
-          ); // TODO(alexandr): show something from 4 motors.
+          );
         })
-        ..voidOnModel<Uint16WithStatusBody,
+        ..voidOnPackage<Uint16WithStatusBody,
             MotorSpeedIncomingDataSourcePackage>((package) {
           emit(
             state.copyWith(
-              speed: package.toIntWithStatus(
-                0,
+              speed: state.speed.updateAt(
+                package.motorIndex,
+                package.dataModel.toIntWithStatus(),
               ),
             ),
-          ); // TODO(alexandr): show something from 4 motors.
+          );
         })
         ..voidOnModel<Uint32WithStatusBody, OdometerIncomingDataSourcePackage>(
             (model) {
